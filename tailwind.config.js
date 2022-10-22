e63a70b8aee9a579b09a29f5d11cf61a2360fa4d
echonabin <58589157+echonabin@users.noreply.github.com>
2022-10-22 12:32:23 +0545

    tailwind css added on UI

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./apps/practitioner-ui/pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
