1491cc93b77a9aa6cbb765d71cc7e133b918554f
echonabin <58589157+echonabin@users.noreply.github.com>
2022-10-24 09:24:42 +0545

    auth store added

---
+++ libs/store/src/lib/auth-store.ts
@@ -0,0 +1,71 @@
+import create from 'zustand';
+
+type authData = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  profileUrl: string;
+  jwtToken: string;
+  refreshToken?: {
+    token: string;
+    expires: string;
+    isActive: boolean;
+  };
+  message?: string;
+};
+
+type registerData = {
+  message: string;
+};
+
+interface authState {
+  data: authData;
+  loading: boolean;
+  registerUser: (data: registerData) => void;
+  loginUser: (data: authData) => void;
+  logoutUser: () => void;
+  refreshToken: (data: authData) => void;
+  setLoading: () => void;
+}
+
+export const useAuthData = create<authState>((set) => ({
+  // Initial state
+  loading: true,
+  data: {} as authData,
+  message: '',
+  registerUser: (data: registerData) => {
+    set((state) => ({
+      ...state,
+      message: data.message,
+      loading: true,
+    }));
+  },
+  loginUser: (data: authData) => {
+    set((state) => ({
+      ...state,
+      data: data,
+      loading: true,
+    }));
+  },
+  logoutUser: () => {
+    set((state) => ({
+      ...state,
+      data: {} as authData,
+      loading: true,
+    }));
+  },
+  refreshToken: (data: authData) => {
+    set((state) => ({
+      ...state,
+      data: data,
+      loading: true,
+    }));
+  },
+  setLoading: () => {
+    set((state) => ({
+      ...state,
+      loading: false,
+    }));
+  },
+}));
---
+++ libs/store/src/lib/index.ts
@@ -0,0 +1,1 @@
+export { useAuthData } from './auth-store';
